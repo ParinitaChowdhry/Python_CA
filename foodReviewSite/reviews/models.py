bdf4f29f172aa9f055c422f3c7adc3d06c16b66c
wynta <wynta@LAPTOP-29I0R6IG>
2019-02-28 15:29:54 +0800

    database

--- foodReviewSite/reviews/models.py
+++ foodReviewSite/reviews/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from django.conf import settings 
 from django.contrib.auth.models import User
 
 # Create your models here.
 class Restaurant (models.Model):
     name = models.CharField(max_length=200)
     address = models.CharField(max_length=200)
-	category_choices = (
+    category_choices = (
     (1,'Western'),
     (2,'Japanese'),
     (3,'Chinese'),
@@ -16,7 +17,7 @@ class Restaurant (models.Model):
 
 class Review (models.Model):
     restaurant= models.ForeignKey(Restaurant, on_delete=models.CASCADE)
-	user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     description= models.CharField(max_length=2000)
     rating = models.FloatField(default=0.00)
     reviewInputDateTime= models.DateTimeField('date published')
@@ -25,8 +26,8 @@ class Comment (models.Model):
     review= models.ForeignKey(Review, on_delete=models.CASCADE)
     content= models.CharField(max_length=1000)
     commentInputDateTime= models.DateTimeField('date published')
-	user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 class Like (models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL)
-    review= models.ForeignKey(Review, on_delete=models.CASCADE
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    review= models.ForeignKey(Review, on_delete=models.CASCADE)
